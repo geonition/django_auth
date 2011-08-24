c08ec5769c3983706e0eb3c8396f13b7565700bd
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-24 22:42:18 +0300

    setup script added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='auth',
+    version='1.0.0',
+    author='Kristoffer Snabb',
+    url='https://github.com/geonition/django_auth',
+    packages=['auth'],
+)
