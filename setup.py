106b0d65dfc2c7135b3176f6a909a58df8ef0168
Kristoffer Snabb <kristoffer.snabb@gmail.com>
2011-08-30 16:20:26 +0300

    setup modified to include the templates

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
 
 setup(
     name='auth',
     version='1.0.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_auth',
-    packages=['auth'],
+    packages=find_packages(),
+    include_package_data=True,
+    package_data = {
+        "auth": [
+            "templates/*.js",
+            "templates/email_templates/*.txt"
+        ],
+    },
+    zip_safe=False,
+    install_requires=['django']
 )
