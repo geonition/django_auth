dd3ecb3f81554b934cd0964dde848c8140e0e14c
Mikko Johansson <mikko.johansson@aalto.fi>
2012-09-26 12:16:04 +0300

    Version number increse to 4.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='gntauth',
-    version='4.0.1',
+    version='4.1.0',
     author='Kristoffer Snabb',
     url='https://github.com/geonition/django_auth',
     packages=find_packages(),
